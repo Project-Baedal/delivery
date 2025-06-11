a26348fc0a90068cb8006c20bc156e5751d1c8c9
handsone-u <handsoneu@gmail.com>
2025-06-11 16:02:06 +0900

    [#2] add: SseEmitterManager 관리 대상의 자료구조 변경
    - list에서 단일 SSeEmitter로 변경함.

--- src/main/java/com/baedal/delivery/adapter/in/web/manager/SseEmitterManager.java
+++ src/main/java/com/baedal/delivery/adapter/in/web/manager/SseEmitterManager.java
@@ -2,56 +2,52 @@ package com.baedal.delivery.adapter.in.web.manager;
 
 import com.baedal.delivery.adapter.in.message.listener.RedisSubscriber;
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SseEmitterManager {
 
   private final RedisSubscriber subscriber;
 
-  // TODO: owner가 하나의 store에 여러 클라이언트로 SSE 연결할 경우 storeId->List로
-  private final Map<Long, List<SseEmitter>> emitterMap = new ConcurrentHashMap<>();
+  private final Map<Long, SseEmitter> emitterMap = new ConcurrentHashMap<>();
 
   public synchronized void addEmitter(Long storeId, SseEmitter emitter) {
-    emitterMap.computeIfAbsent(storeId, id -> new CopyOnWriteArrayList<>()).add(emitter);
+    if (!emitterMap.containsKey(storeId)) {
+      emitterMap.put(storeId, emitter);
 
-    if (emitterMap.get(storeId).size() == 1) {
       subscriber.subscribe(storeId);
-    }
 
-    emitter.onCompletion(() -> removeEmitter(storeId, emitter));
-    emitter.onTimeout(() -> removeEmitter(storeId, emitter));
+      emitter.onCompletion(() -> removeEmitter(storeId, "completion"));
+      emitter.onTimeout(() -> removeEmitter(storeId, "timeout"));
+    }
   }
 
-  public synchronized void removeEmitter(Long storeId, SseEmitter emitter) {
-    List<SseEmitter> emitters = emitterMap.get(storeId);
-    if (emitters != null) {
-      emitters.remove(emitter);
-      if (emitters.isEmpty()) {
-        emitterMap.remove(storeId);
-        subscriber.unsubscribe(storeId);
-      }
+  public synchronized void removeEmitter(Long storeId, String reason) {
+    SseEmitter emitter = emitterMap.get(storeId);
+    if (emitter != null) {
+      emitterMap.remove(storeId);
+      subscriber.unsubscribe(storeId);
     }
+    log.debug("Removing emitter {}", reason);
   }
 
   public void sendToClients(Long storeId, String message) {
-    List<SseEmitter> emitters = emitterMap.get(storeId);
-    if (emitters == null) return;
+    SseEmitter emitter = emitterMap.get(storeId);
+    if (emitter == null) {
+      return;
+    }
 
-    for (SseEmitter emitter : emitters) {
-      try {
-        emitter.send(SseEmitter.event().name("delivery-update").data(message));
-      } catch (IOException e) {
-        removeEmitter(storeId, emitter);
-      }
+    try {
+      emitter.send(SseEmitter.event().name("delivery-update").data(message));
+    } catch (IOException e) {
+      removeEmitter(storeId, e.toString());
     }
   }
-
 }
