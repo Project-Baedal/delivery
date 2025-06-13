c88d9669dc04d41324ef300528cfb0076326e1df
handsone-u <handsoneu@gmail.com>
2025-06-13 15:12:21 +0900

    [#2] add: redis publisher 파라미터 타입 변경

--- src/main/java/com/baedal/delivery/adapter/out/message/DeliveryMessagePublisherAdapter.java
+++ src/main/java/com/baedal/delivery/adapter/out/message/DeliveryMessagePublisherAdapter.java
@@ -2,12 +2,10 @@ package com.baedal.delivery.adapter.out.message;
 
 import com.baedal.delivery.adapter.out.message.publisher.RedisPublisher;
 import com.baedal.delivery.application.port.out.DeliveryMessagePublisherPort;
-import com.baedal.delivery.domain.model.DeliveryStatus;
 import com.baedal.delivery.domain.model.UpdateDeliveryStatus;
 import com.baedal.delivery.util.ObjectMapperUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,9 +19,8 @@ public class DeliveryMessagePublisherAdapter implements DeliveryMessagePublisher
 
   public void deliveryStatusUpdate(Long storeId, UpdateDeliveryStatus model) {
     String channelName = String.format(channelFormat, storeId);
-    ChannelTopic topic = new ChannelTopic(channelName);
     String message = ObjectMapperUtil.toJson(model);
 
-    redisPublisher.publish(topic, message);
+    redisPublisher.publish(channelName, message);
   }
 }
--- src/main/java/com/baedal/delivery/adapter/out/message/publisher/RedisPublisher.java
+++ src/main/java/com/baedal/delivery/adapter/out/message/publisher/RedisPublisher.java
@@ -3,7 +3,6 @@ package com.baedal.delivery.adapter.out.message.publisher;
 import com.baedal.delivery.util.ObjectMapperUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,8 +11,8 @@ public class RedisPublisher {
 
   private final RedisTemplate<String, Object> template;
 
-  public void publish(ChannelTopic topic, Object message) {
+  public void publish(String channelName, Object message) {
     String json = ObjectMapperUtil.toJson(message);
-    template.convertAndSend(topic.getTopic(), json);
+    template.convertAndSend(channelName, json);
   }
 }
